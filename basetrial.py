6902fe5b0b15f118c03ef457cc89a2ce12775d5f
FaficzekRK <rafal.aleksander.kliber@gmail.com>
2025-02-18 21:43:04 +0000

    Spur of inspiration
    Setting some ground work

--- basetrial.py
+++ basetrial.py
@@ -2,5 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class particle:
-    def __init__(self):
-        pass
+    def __init__(self, position, velocity, time, spin, weight, MagField):
+        self.position = position
+        self.velocity = velocity
+
+egg = particle()
+arra = np.array(1,2,3)
+print(type(arra))

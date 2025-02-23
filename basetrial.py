8421b4cb1ab3d0a2188a7e41a0946af4bce5afaa
FaficzekRK <rafal.aleksander.kliber@gmail.com>
2025-02-23 23:38:43 +0000

    Update basetrial.py

--- basetrial.py
+++ basetrial.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class particle:
-    def __init__(self, position, velocity, time, spin, weight, MagField):
-        self.position = position
-        self.velocity = velocity
+#class particle:
+#    def __init__(self, position, velocity, time, spin, weight, MagField):
+#        self.position = position
+ #       self.velocity = velocity
 
-egg = particle()
-arra = np.array(1,2,3)
+#egg = particle()
+arra = np.array((1,2,3))
 print(type(arra))
+print(arra)
